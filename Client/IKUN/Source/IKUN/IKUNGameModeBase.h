bc9a5e45bfb6e9fd2f2dd2e43d2b526c49c20164
481381521@qq.com <481381521@qq.com>
2023-04-09 19:26:49 +0800

    Init Client Project

---
+++ Client/IKUN/Source/IKUN/IKUNGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "IKUNGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class IKUN_API AIKUNGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
