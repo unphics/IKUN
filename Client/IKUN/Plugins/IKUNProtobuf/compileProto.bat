b124ab5c902a787c03c55c751bfb00ccbd7111c7
481381521@qq.com <481381521@qq.com>
2023-04-15 22:33:10 +0800

    protobuf-1.0-vcpkg

--- Client/IKUN/Plugins/IKUNProtobuf/compileProto.bat
+++ Client/IKUN/Plugins/IKUNProtobuf/compileProto.bat
@@ -1,6 +1,6 @@
 @echo off
 
-%VCPKG_ROOT%/packages/protobuf_x64-windows/tools/protobuf/protoc.exe --proto_path=../../Source/IKUNProtos/proto/ --cpp_out=../../Source/IKUNProtos/Public/ Test.proto
+::%VCPKG_ROOT%/packages/protobuf_x64-windows/tools/protobuf/protoc.exe --proto_path=../../Source/IKUNProtos/proto/ --cpp_out=../../Source/IKUNProtos/Public/ Test.proto
 
 :: /r表示自动遍历子目录，%%i表示声明一个i，in ()表示遍历的目录，%VCPKG_ROOT%是环境变量，
 :: for /r %%i in (../../Source/IKUNProtos/proto/*.proto) do (
@@ -10,5 +10,6 @@
 :: echo %VCPKG_ROOT%
 :: /s表示复制子目录，/y表示不提示是否覆盖已有文件
 :: xcopy ../../Source/IKUNProtos/Public/*.cc ../../Source/IKUNProtos/Private/ /S /E /Y
+
 
 pause
