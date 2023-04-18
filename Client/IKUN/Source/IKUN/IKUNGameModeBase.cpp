d03300f5ed1bff8915420c568dcd4c1d562a5cc3
481381521@qq.com <481381521@qq.com>
2023-04-18 11:32:16 +0800

    p

--- Client/IKUN/Source/IKUN/IKUNGameModeBase.cpp
+++ Client/IKUN/Source/IKUN/IKUNGameModeBase.cpp
@@ -17,9 +17,10 @@ void AIKUNGameModeBase::StartPlay() {
 	UPBMgr* mgr = NewObject<UPBMgr>();
 	this->psr = mgr->p;
 	(*psr).set_page_number(9898998);
-	(*psr).set_query("xiao hei zi");
-	FString str = psr->query().c_str();
-	std::cout << "str::::::" << psr->query() << std::endl;
-	UE_LOG(LogTemp, Display, TEXT("========== str: %d"), psr->page_number());
+
+	(*psr).set_query(std::string("xiaoheizi"));
+	//FString str = psr->query().c_str();
+	//std::cout << "str::::::" << psr->query() << std::endl;
+	//UE_LOG(LogTemp, Display, TEXT("========== str: %d"), psr->page_number());
 	//SearchRequest* obj = mgr->PBNew<SearchRequest>(std::string("SearchRequest"));
 }
