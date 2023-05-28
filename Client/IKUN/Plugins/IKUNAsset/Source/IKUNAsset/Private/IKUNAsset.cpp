31337f9fbe3e903315aa78f3b8f0003edfff8e86
unphics <481381521@qq.com>
2023-05-28 23:21:52 +0800

    悟空

--- Client/IKUN/Plugins/IKUNAsset/Source/IKUNAsset/Private/IKUNAsset.cpp
+++ Client/IKUN/Plugins/IKUNAsset/Source/IKUNAsset/Private/IKUNAsset.cpp
@@ -1,20 +1,7 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
-#include "IKUNAsset.h"
 
-#define LOCTEXT_NAMESPACE "FIKUNAssetModule"
-
-void FIKUNAssetModule::StartupModule()
-{
-	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+#include "..\Public\IKUNAsset.h"
+int32 UIKUNAsset::getId() {
+	return this->id;
 }
-
-void FIKUNAssetModule::ShutdownModule()
-{
-	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
-	// we call this function before unloading the module.
-}
-
-#undef LOCTEXT_NAMESPACE
-	
-IMPLEMENT_MODULE(FIKUNAssetModule, IKUNAsset)
