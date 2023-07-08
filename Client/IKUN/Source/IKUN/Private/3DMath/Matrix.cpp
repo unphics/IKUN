bf09cb9fea5ef1b68ad8a6005ff8373bcedca281
unphics <481381521@qq.com>
2023-07-08 19:10:16 +0800

    get bp asset meta info

--- Client/IKUN/Source/IKUN/Private/3DMath/Matrix.cpp
+++ Client/IKUN/Source/IKUN/Private/3DMath/Matrix.cpp
@@ -2,15 +2,11 @@
 
 void Matrix::func() {
 	FMatrix m1;
-	m1.M[0][0] = 1.f;
-	m1.M[0][1] = 2.f;
-	m1.M[1][0] = 3.f;
-	m1.M[1][2] = 4.f;
+	m1.M[0][0] = 1.f; m1.M[0][1] = 2.f;
+	m1.M[1][0] = 3.f; m1.M[1][2] = 4.f;
 	FMatrix m2;
-	m2.M[0][0] = 4.f;
-	m2.M[0][1] = 3.f;
-	m2.M[1][0] = 2.f;
-	m2.M[1][2] = 1.f;
+	m2.M[0][0] = 4.f; m2.M[0][1] = 3.f;
+	m2.M[1][0] = 2.f; m2.M[1][2] = 1.f;
 
 	FMatrix m3 = add(m1, m2);
 	FMatrix m4 = m1 + m2;
@@ -27,42 +23,18 @@ void Matrix::func() {
 
 FMatrix Matrix::add(FMatrix a, FMatrix b) {
 	FMatrix mtx;
-	mtx.M[0][0] = a.M[0][0] + b.M[0][0];
-	mtx.M[1][0] = a.M[1][0] + b.M[1][0];
-	mtx.M[2][0] = a.M[2][0] + b.M[2][0];
-	mtx.M[3][0] = a.M[3][0] + b.M[3][0];
-	mtx.M[0][1] = a.M[0][1] + b.M[0][1];
-	mtx.M[1][1] = a.M[1][1] + b.M[1][1];
-	mtx.M[2][1] = a.M[2][1] + b.M[2][1];
-	mtx.M[3][1] = a.M[3][1] + b.M[3][1];
-	mtx.M[0][2] = a.M[0][2] + b.M[0][2];
-	mtx.M[1][2] = a.M[1][2] + b.M[1][2];
-	mtx.M[2][2] = a.M[2][2] + b.M[2][2];
-	mtx.M[3][2] = a.M[3][2] + b.M[3][2];
-	mtx.M[0][3] = a.M[0][3] + b.M[0][3];
-	mtx.M[1][3] = a.M[1][3] + b.M[1][3];
-	mtx.M[2][3] = a.M[2][3] + b.M[2][3];
-	mtx.M[3][3] = a.M[3][3] + b.M[3][3];
+	mtx.M[0][0]=a.M[0][0]+b.M[0][0]; mtx.M[1][0]=a.M[1][0]+b.M[1][0]; mtx.M[2][0]=a.M[2][0]+b.M[2][0]; mtx.M[3][0]=a.M[3][0]+b.M[3][0];
+	mtx.M[0][1]=a.M[0][1]+b.M[0][1]; mtx.M[1][1]=a.M[1][1]+b.M[1][1]; mtx.M[2][1]=a.M[2][1]+b.M[2][1]; mtx.M[3][1]=a.M[3][1]+b.M[3][1];
+	mtx.M[0][2]=a.M[0][2]+b.M[0][2]; mtx.M[1][2]=a.M[1][2]+b.M[1][2]; mtx.M[2][2]=a.M[2][2]+b.M[2][2]; mtx.M[3][2]=a.M[3][2]+b.M[3][2];
+	mtx.M[0][3]=a.M[0][3]+b.M[0][3]; mtx.M[1][3]=a.M[1][3]+b.M[1][3]; mtx.M[2][3]=a.M[2][3]+b.M[2][3]; mtx.M[3][3]=a.M[3][3]+b.M[3][3];
 	return mtx;
 }
 FMatrix Matrix::cross(FMatrix a, float b) {
 	FMatrix mtx;
-	mtx.M[0][0] = a.M[0][0] * b;
-	mtx.M[1][0] = a.M[1][0] * b;
-	mtx.M[2][0] = a.M[2][0] * b;
-	mtx.M[3][0] = a.M[3][0] * b;
-	mtx.M[0][1] = a.M[0][1] * b;
-	mtx.M[1][1] = a.M[1][1] * b;
-	mtx.M[2][1] = a.M[2][1] * b;
-	mtx.M[3][1] = a.M[3][1] * b;
-	mtx.M[0][2] = a.M[0][2] * b;
-	mtx.M[1][2] = a.M[1][2] * b;
-	mtx.M[2][2] = a.M[2][2] * b;
-	mtx.M[3][2] = a.M[3][2] * b;
-	mtx.M[0][3] = a.M[0][3] * b;
-	mtx.M[1][3] = a.M[1][3] * b;
-	mtx.M[2][3] = a.M[2][3] * b;
-	mtx.M[3][3] = a.M[3][3] * b;
+	mtx.M[0][0] = a.M[0][0]*b; mtx.M[1][0] = a.M[1][0]*b; mtx.M[2][0] = a.M[2][0]*b; mtx.M[3][0] = a.M[3][0]*b;
+	mtx.M[0][1] = a.M[0][1]*b; mtx.M[1][1] = a.M[1][1]*b; mtx.M[2][1] = a.M[2][1]*b; mtx.M[3][1] = a.M[3][1]*b;
+	mtx.M[0][2] = a.M[0][2]*b; mtx.M[1][2] = a.M[1][2]*b; mtx.M[2][2] = a.M[2][2]*b; mtx.M[3][2] = a.M[3][2]*b;
+	mtx.M[0][3] = a.M[0][3]*b; mtx.M[1][3] = a.M[1][3]*b; mtx.M[2][3] = a.M[2][3]*b; mtx.M[3][3] = a.M[3][3]*b;
 	return mtx;
 }
 FMatrix Matrix::dot(FMatrix a, FMatrix b) {
@@ -89,42 +61,18 @@ FMatrix Matrix::dot(FMatrix a, FMatrix b) {
 
 FMatrix Matrix::transpose(FMatrix a) {
 	FMatrix mtx;
-	mtx.M[0][0] = a.M[0][0];
-	mtx.M[1][0] = a.M[0][1];
-	mtx.M[2][0] = a.M[0][2];
-	mtx.M[3][0] = a.M[0][3];
-	mtx.M[0][1] = a.M[1][0];
-	mtx.M[1][1] = a.M[1][1];
-	mtx.M[2][1] = a.M[1][2];
-	mtx.M[3][1] = a.M[1][3];
-	mtx.M[0][2] = a.M[2][0];
-	mtx.M[1][2] = a.M[2][1];
-	mtx.M[2][2] = a.M[2][2];
-	mtx.M[3][2] = a.M[2][3];
-	mtx.M[0][3] = a.M[3][0];
-	mtx.M[1][3] = a.M[3][1];
-	mtx.M[2][3] = a.M[3][2];
-	mtx.M[3][3] = a.M[3][3];
+	mtx.M[0][0] = a.M[0][0]; mtx.M[1][0] = a.M[0][1]; mtx.M[2][0] = a.M[0][2]; mtx.M[3][0] = a.M[0][3];
+	mtx.M[0][1] = a.M[1][0]; mtx.M[1][1] = a.M[1][1]; mtx.M[2][1] = a.M[1][2]; mtx.M[3][1] = a.M[1][3];
+	mtx.M[0][2] = a.M[2][0]; mtx.M[1][2] = a.M[2][1]; mtx.M[2][2] = a.M[2][2]; mtx.M[3][2] = a.M[2][3];
+	mtx.M[0][3] = a.M[3][0]; mtx.M[1][3] = a.M[3][1]; mtx.M[2][3] = a.M[3][2]; mtx.M[3][3] = a.M[3][3];
 	return mtx;
 }
 
 FMatrix Matrix::getUnit() {
 	FMatrix mtx;
-	mtx.M[0][0] = 1;
-	mtx.M[1][0] = 0;
-	mtx.M[2][0] = 0;
-	mtx.M[3][0] = 0;
-	mtx.M[0][1] = 0;
-	mtx.M[1][1] = 1;
-	mtx.M[2][1] = 0;
-	mtx.M[3][1] = 0;
-	mtx.M[0][2] = 0;
-	mtx.M[1][2] = 0;
-	mtx.M[2][2] = 1;
-	mtx.M[3][2] = 0;
-	mtx.M[0][3] = 0;
-	mtx.M[1][3] = 0;
-	mtx.M[2][3] = 0;
-	mtx.M[3][3] = 1;
+	mtx.M[0][0] = 1; mtx.M[1][0] = 0; mtx.M[2][0] = 0; mtx.M[3][0] = 0;
+	mtx.M[0][1] = 0; mtx.M[1][1] = 1; mtx.M[2][1] = 0; mtx.M[3][1] = 0;
+	mtx.M[0][2] = 0; mtx.M[1][2] = 0; mtx.M[2][2] = 1; mtx.M[3][2] = 0;
+	mtx.M[0][3] = 0; mtx.M[1][3] = 0; mtx.M[2][3] = 0; mtx.M[3][3] = 1;
 	return mtx;
 }
--- Client/IKUN/Source/IKUN/Public/3DMath/Matrix.h
+++ Client/IKUN/Source/IKUN/Public/3DMath/Matrix.h
@@ -9,4 +9,6 @@ public:
 	static FMatrix dot(FMatrix a, FMatrix b);
 	static FMatrix transpose(FMatrix a); // 转置
 	static FMatrix getUnit();
+	// 矩阵没有除法，但是有除法的逆运算（限定正方形矩阵）
+	static FMatrix inverse();
 };
